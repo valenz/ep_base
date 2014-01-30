219b0bbf28d7f147dc8575a879733f61b9d72139
stephan <vlaenzr@googlemail.com>
2014-01-30 06:28:31 -0500

    add first script

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-exports.FUNCTIONNAME1 = function(hook, context){
+exports.expressCreateServer = function(hook_name, args, cb) {
+    args.app.get('apples', function(req, res) {
+        res.send("<em>Abra Cadabra!</em>");
+    });
 }
