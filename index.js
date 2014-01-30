0f6948d515fa1ff7e1565f102f9f51916d4f566c
stephan <vlaenzr@googlemail.com>
2014-01-30 07:54:07 -0500

    updated some files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.expressCreateServer = function(hook_name, args, cb) {
-    args.app.get('apples', function(req, res) {
+    args.app.get('/apples', function(req, res) {
         res.send("<em>Abra Cadabra!</em>");
     });
 }
