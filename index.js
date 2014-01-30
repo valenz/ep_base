eefacda930010449d3dcfb9506193484a5ba8cd0
stephan <vlaenzr@googlemail.com>
2014-01-30 09:01:16 -0500

    update some files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.registerRoute = function (hook_name, args, cb) {
-  args.app.get('/admin/pads', function(req, res) {
+  args.app.get('/admin/managepads', function(req, res) {
     
     var render_args = {
       errors: []
