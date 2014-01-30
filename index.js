f3fb6dbef3baad3f686b5efe1664673ab8b4d261
stephan <vlaenzr@googlemail.com>
2014-01-30 08:56:59 -0500

    update some files

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-exports.expressCreateServer = function(hook_name, args, cb) {
+var eejs = require('ep_etherpad-lite/node/eejs');
+
+exports.registerRoute = function (hook_name, args, cb) {
+  args.app.get('/admin/pads', function(req, res) {
+    
+    var render_args = {
+      errors: []
+    };
+    res.send( eejs.require("ep_adminpads/templates/admin/pads.html", render_args) );
+  });
+};
+
+/*exports.expressCreateServer = function(hook_name, args, cb) {
     args.app.get('/apples', function(req, res) {
         res.send("<em>Abra Cadabra!</em>");
     });
-}
+}*/
