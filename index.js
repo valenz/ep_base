34c841f6500fd3f4f72144fd4bb4985d64a51cee
stephan <vlaenzr@googlemail.com>
2014-01-30 09:29:17 -0500

    update some files

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ exports.registerRoute = function (hook_name, args, cb) {
     var render_args = {
       errors: []
     };
-    res.send( eejs.require("ep_adminpads/templates/admin/pads.html", render_args) );
+    res.send( eejs.require("ep_managepads/templates/admin/managepads.html", render_args) );
   });
+};
+
+exports.eejsBlock_adminMenu = function (hook_name, args, cb) {
+    var hasAdminUrlPrefix = (args.content.indexOf('<a href="admin/') != -1)
+    , hasOneDirDown = (args.content.indexOf('<a href="../') != -1)
+    , hasTwoDirDown = (args.content.indexOf('<a href="../../') != -1)
+    , urlPrefix = hasAdminUrlPrefix ? "admin/" : hasTwoDirDown ? "../../" : hasOneDirDown ? "../" : ""
+    ;
+    
+    args.content = args.content + '<li><a href="'+ urlPrefix +'pads">Manage pads</a> </li>';
+    return cb();
 };
 
 /*exports.expressCreateServer = function(hook_name, args, cb) {
