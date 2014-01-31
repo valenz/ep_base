9306c239b2598010e2b214365778bc1de7532d71
stephan <vlaenzr@googlemail.com>
2014-01-31 09:01:56 -0500

    updated some files

--- static/js/admin/managepads.js
+++ static/js/admin/managepads.js
@@ -1,15 +1,3 @@
 exports.documentReady = function(hooks, context, cb) {
-    if(context != 'admin/managepads') return cb;
     
-    var socket,
-    loc = document.location,
-    port = loc.port == "" ? (loc.protocol == "https:" ? 443 : 80) : loc.port,
-    url = loc.protocol + "//" + loc.hostname + ":" + port + "/",
-    pathComponents = location.pathname.split('/'),
-    // Strip admin/plugins
-    baseURL = pathComponents.slice(0,pathComponents.length-2).join('/') + '/',
-    resource = baseURL.substring(1) + "socket.io";
-    
-    //connect
-    socket = io.connect(url, {resource : resource}).of("/pluginfw/admin/managepads");
 }
